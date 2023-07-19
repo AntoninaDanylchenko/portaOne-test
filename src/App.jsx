cb7169595c9e2724282055365be9b02cfee64e92
TosiaDan <antonina.ivanets@gmail.com>
2023-07-19 17:10:36 +0200

    ADD: do with react logic and ADD favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState("");
+
+  const uniqueLetter = (text) => {
+    const letters = text
+      .split(" ")
+      .map((word) =>
+        word.split("").find((e) => word.indexOf(e) === word.lastIndexOf(e))
+      )
+      .filter((e) => e);
+    return letters.find((e) => letters.indexOf(e) === letters.lastIndexOf(e));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Find your Unique symbol</h1>
+      <textarea
+        rows={10}
+        cols={50}
+        placeholder="Enter you text"
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+
+      {text && (
+        <>
+          {uniqueLetter(text) ? (
+            <p className="answer">
+              Unique symbol is{" "}
+              <span className="answer-span">{uniqueLetter(text)}</span>
+            </p>
+          ) : (
+            <p className="answer">Null unique symbol </p>
+          )}
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
